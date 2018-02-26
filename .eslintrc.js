bb884ee3dd8e3a7249ab8d4d795a5a4938db4a8f
Eduardo Tarancón Delgado <eduardo.tarancon.contractor@bbva.com>
2018-02-26 12:03:26 +0100

    [MCH][DB-85440][feat] eslint cofigured and jshint removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-	extends: 'eslint-config-bbva'
+  extends: 'eslint-config-bbva',
+  env: {
+		browser: true
+	}
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 var readEnvironmentConfig = require('./lib/environment-config').read;
